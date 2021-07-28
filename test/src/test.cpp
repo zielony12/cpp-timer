049419b1109ed7131db46a8abc9815371687c596
zielony12 <xdzielony@gmail.com>
2021-07-28 21:31:18 +0200

    Added latest binaries and object files

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,10 +1,10 @@
-#include <timer/Countdown.hpp>	// include Countdown class
-#include <timer/Stopwatch.hpp>	// include Stopwatch class
+#include <timer/Countdown.hpp>
+#include <timer/Stopwatch.hpp>
 
 int main() {
-	tmr::Countdown c1("Countdown timer 1", 0, 0, 5); // create and set the timer to 5 seconds
-	c1.start(); // start the countdown timer
-	tmr::Stopwatch s1("Stopwatch timer 1"); // useless without threads
-	s1.start(); // start the stopwatch timer
+	tmr::Countdown c1("Countdown timer 1", 0, 0, 5);
+	c1.start();
+	tmr::Stopwatch s1("Stopwatch timer 1");
+	s1.start();
 	return 0;
 }
